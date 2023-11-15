0bd699f6f4bf451b8fbb7e690b8d847b10cf055f
NastyaOu <onufrienko869@gmail.com>
2023-11-15 14:34:27 +0300

    Проверка на сервере

--- server/app.js
+++ server/app.js
@@ -18,6 +18,49 @@ app.get('/accounts', (req, res) => {
 })
 
 app.post('/accounts', (req, res) => {
+	const usernameRegExp = /^[а-яА-ЯёЁa-zA-Z0-9]+$/
+	const passwordRegExp =
+		/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/g
+
+	if (!req.body.username) {
+		res.status(400).json('Не хватает логина')
+		return
+	}
+	const username = req.body.username
+
+	if (username.length < 4 || username.length > 16) {
+		res.status(400).json('Длина логина должна составлять от 4 до 16 символов')
+		return
+	}
+
+	if (!usernameRegExp.test(username)) {
+		res.status(400).json('Логин не должен содержать специальных символов')
+
+		return
+	}
+
+	if (!req.body.password) {
+		res.status(400).json('Не хватает пароля')
+		return
+	}
+	const password = req.body.password
+
+	if (password.length < 4 || password.length > 16) {
+		res.status(400).json('Длина пароля должна составлять от 4 до 16 символов')
+		return
+	}
+
+	if (!passwordRegExp.test(password)) {
+		res.status(400)
+			.json(`Пароль должен состоять из латинских символов и включать:
+       - Хотя бы один символ в верхнем регистре
+       - Хотя бы один символ в нижнем регистре
+       - Хотя бы одно число
+       - Хотя бы один спецсимвол`)
+
+		return
+	}
+
 	users.push(new User(req.body.username, req.body.password))
 	res.status(201).json(req.body)
 })
